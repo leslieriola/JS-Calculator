9717989f3351e884cf2a05eb98fb3d4f42c1ff53
leslieriola <leslieannampigriola@gmail.com>
2023-04-20 17:52:18 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+const numberButtons = document.querySelectorAll(".is-num");
+const operatorButtons = document.querySelectorAll(".is-operator");
+const clearButton = document.querySelector(".is-clear");
+const largeDisplay = document.querySelector(".large-display");
+const smallDisplay = document.querySelector(".small-display");
+const btnEquals = document.querySelector(".is-equals");
+const dotButton = document.querySelector(".is-decimal");
+const deleteButton = document.querySelector(".is-delete");
+
+let currentInput = "";
+let previousInput = "";
+let operator = undefined;
+
+function clear() {
+  currentInput = "";
+  previousInput = "";
+  operator = undefined;
+}
+
+function del() {
+  currentInput = currentInput.toString().slice(0, -1);
+}
+
+function updateDisplay() {
+  if (currentInput === "" && largeDisplay.value === "") {
+    largeDisplay.value = "";
+  } else {
+    largeDisplay.value = currentInput;
+  }
+
+  if (operator !== undefined) {
+    smallDisplay.value = previousInput + operator;
+  } else {
+    smallDisplay.value = "";
+  }
+}
+
+function chooseOperator(chosenOperator) {
+  if (currentInput === "") return;
+  if (previousInput !== "") compute();
+
+  operator = chosenOperator;
+  previousInput = currentInput;
+  currentInput = "";
+}
+
+function appendNumber(number) {
+  currentInput += number;
+}
+
+function compute() {
+  let total = 0;
+  const previousNumber = parseInt(previousInput);
+  const currentNumber = parseInt(currentInput);
+
+  switch (operator) {
+    case "+":
+      total = previousNumber + currentNumber;
+      break;
+    case "−":
+      total = previousNumber - currentNumber;
+      break;
+    case "×":
+      total = previousNumber * currentNumber;
+      break;
+    case "÷":
+      total = previousNumber / currentNumber;
+      break;
+    default:
+      return "";
+  }
+
+  currentInput = total;
+  previousInput = "";
+  operator = undefined;
+}
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (largeDisplay.value === "" && event.target.textContent === "0") return;
+
+    appendNumber(event.target.textContent);
+    updateDisplay();
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  clear();
+  updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  del();
+  updateDisplay();
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    chooseOperator(event.target.textContent);
+    updateDisplay();
+  });
+});
+
+btnEquals.addEventListener("click", () => {
+  compute();
+  updateDisplay();
+});
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
